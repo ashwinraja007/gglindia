c5a284fe564c8294dcc1d9776b35d665b100a7b7
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-06-24 17:01:53 +0530

    Update TermsOfUse.tsx

--- src/pages/TermsOfUse.tsx
+++ src/pages/TermsOfUse.tsx
@@ -18,28 +18,28 @@ const TermsOfUsePage: React.FC = () => {
           of Use, refrain from accessing or utilizing the Website.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">User Eligibility</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">User Eligibility</h2>
         <p>
           GGL (India) provides the Website exclusively to entities and individuals who have reached the legal age of majority
           and have the legal capacity to enter into binding agreements under the applicable law in India. If you do not meet these criteria, you are not authorized to use the Website.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">Scope of Terms of Use</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Scope of Terms of Use</h2>
         <p>
           These Terms of Use govern your use of the Website and all related applications, software, and services, collectively referred to as "Services," except where a separate agreement applies. Specific terms or agreements, known as "Service Agreements," may apply to certain Services or items provided through the Website. These Service Agreements will either accompany the relevant Services or be accessible via an associated hyperlink.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">Modifications</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Modifications</h2>
         <p>
           GGL (India) reserves the right to modify these Terms of Use at any time. Your continued use of the Website after any changes signifies your acceptance of such modifications. GGL (India) may alter, supplement, delete, or update any aspect of the Website without notice and may adjust or introduce fees for products and services at its sole discretion. General practices and restrictions regarding other GGL (India) products and services may also be established or modified at any time, based on GGL (India)'s sole discretion.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">GGL (India) Privacy Notice</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">GGL (India) Privacy Notice</h2>
         <p>
           By providing any individual's personal information to GGL (India), you affirm that you have obtained all necessary consents for the processing of such personal information as required by the Services.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">License and Ownership</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">License and Ownership</h2>
         <p>
           All intellectual property rights ("Intellectual Property") related to the Website and its contents (the "Content") are the exclusive property of GGL (India), its affiliates, or third parties, and are protected by intellectual property laws in India and globally. Various elements of the Website are also protected by trade name, trade secret, unfair competition, and other laws, prohibiting any copying or imitation. All custom graphics, icons, and other items on the Website, referred to as trademarks, service marks, or trade names ("Marks"), are the property of GGL (India), its affiliates, or entities granting GGL (India) the right to use these Marks. Unauthorized use or interference with these Marks is strictly prohibited without express written consent from GGL (India). Unless expressly authorized by these Terms of Use, you may not copy, reproduce, modify, lease, loan, sell, create derivative works from, upload, transmit, or distribute GGL (India)'s Intellectual Property without prior written permission from GGL (India) or the appropriate third party.
         </p>
@@ -48,9 +48,9 @@ const TermsOfUsePage: React.FC = () => {
           GGL (India) grants you a limited, personal, non-transferable, non-sublicensable, and revocable license to access and use the Website, Content, and Services ("GGL (India) Systems") as presented by GGL (India) and only as expressly permitted. Apart from this limited license, GGL (India) does not transfer any interest in the GGL (India) Systems, information or data available via the GGL (India) Systems (the "Information"), Content, Services, Website, or any other GGL (India) property by allowing access to the Website. Except where required by law or as expressly provided herein, none of the Content or Information may be reverse-engineered, modified, reproduced, republished, translated, retransmitted, resold, or redistributed without prior written consent from GGL (India). Unauthorized actions such as making, selling, offering for sale, modifying, reproducing, displaying, publicly performing, importing, distributing, retransmitting, or otherwise using the Content are strictly prohibited unless expressly permitted by GGL (India).
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">Restrictions on Website Use</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Restrictions on Website Use</h2>
         <p>In addition to the limitations outlined in these Terms of Use, you agree to the following:</p>
-        <ul className="list-disc ml-6 mt-2">
+        <ul className="text-xl font-semibold mt-6 mb-2">
           <li>You must not conceal the origin of information transmitted through the Website.</li>
           <li>You shall refrain from placing false or misleading information on the Website.</li>
           <li>Usage or access of any service, information, application, or software available via the Website is restricted to methods expressly permitted by GGL (India).</li>
@@ -60,14 +60,14 @@ const TermsOfUsePage: React.FC = () => {
           <li>Framing or employing framing techniques to enclose any portion or aspect of the Content or Information without the express written consent of GGL (India) is prohibited.</li>
         </ul>
 
-        <h2 className="list-disc ml-6 mt-2">Outbound Links</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Outbound Links</h2>
         <p>
           The Website may contain links to third-party websites and resources ("Linked Sites"). These links are provided for your convenience and do not signify GGL (India)'s endorsement of their content. GGL (India) does not guarantee the accuracy, reliability, performance, or quality of any content, software, service, or application found on Linked Sites. GGL (India) is not responsible for the availability of Linked Sites or their content and activities. Access to Linked Sites is at your own risk and is governed by the terms, conditions, and privacy policies of those sites.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">Inbound Links</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Inbound Links</h2>
         <p>Linking to any page of the Website other than the homepage through a plain text link is strictly prohibited without a separate linkage agreement with GGL (India). Websites or other entities linking to the homepage or any of its pages are prohibited from:</p>
-        <ul className="list-disc ml-6 mt-2">
+        <ul className="text-xl font-semibold mt-6 mb-2">
           <li>Duplicating Content.</li>
           <li>Using a frame or border around the Content.</li>
           <li>Implying endorsement by GGL (India) or its affiliates.</li>
@@ -76,11 +76,11 @@ const TermsOfUsePage: React.FC = () => {
           <li>Using any logo or mark of GGL (India) or its affiliates without express written permission from GGL (India).</li>
         </ul>
 
-        <h2 className="list-disc ml-6 mt-2">Termination</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Termination</h2>
         <p>
           GGL (India) reserves the right to terminate or suspend your use of the Website, GGL (India) Systems, Information, Services, and Content at its sole discretion, without prior notice or reason. Even if access remains available to others, GGL (India) may suspend or terminate your access at any time. Upon termination or suspension, you must:
         </p>
-        <ul className="list-disc ml-6 mt-2">
+        <ul className="text-xl font-semibold mt-6 mb-2">
           <li>Immediately cease using the Website.</li>
           <li>Destroy any copies of the Content you have made.</li>
         </ul>
@@ -91,17 +91,17 @@ const TermsOfUsePage: React.FC = () => {
           GGL (India) is not liable to you or any third party for terminating or suspending your access to the Website, GGL (India) Systems, Information, and/or Services.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">Disclaimer of Warranties</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Disclaimer of Warranties</h2>
         <p>
           GGL (India) makes no guarantees regarding the results you may achieve by using the Website, GGL (India) Systems, Services, Information, or Content. You use these elements at your own risk, and they are provided "as is." To the fullest extent permitted by law, GGL (India), along with its licensors and suppliers, disclaims all warranties, whether express, implied, statutory, or otherwise. This includes, but is not limited to, warranties of merchantability, non-infringement, and fitness for a particular purpose. GGL (India) and its affiliates, licensors, and suppliers do not guarantee the accuracy, completeness, security, or timeliness of the Content, Information, or Services provided on the Website or GGL (India) Systems. No information obtained from the Website creates any warranty that is not expressly stated in these Terms of Use.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">Limitation of Liability</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Limitation of Liability</h2>
         <p>
           To the extent permitted by law, GGL (India), its affiliates, licensors, suppliers, or any third parties mentioned on the Website are not liable for any incidental, direct, indirect, exemplary, punitive, and/or consequential damages, lost profits, or damages resulting from lost data or business interruption caused by the use or inability to use the Website, GGL (India) Systems, Information, Services, or Content. This limitation applies regardless of the legal theory on which the claim is based, whether warranty, contract, tort, delict, or any other, and even if GGL (India) has been advised of the possibility of such damages. To the extent allowed by law, the remedies provided to you in these Terms of Use are exclusive and limited to those expressly stated herein.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">Compliance with Laws, Including Export Control</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Compliance with Laws, Including Export Control</h2>
         <p>
           You agree to use the GGL (India) Website in full compliance with all applicable laws, regulations, and rules. Your actions should not, as determined solely by GGL (India), harm GGL (India)'s goodwill or reputation. Additionally, you must not engage in any activities that could cause GGL (India) to violate any relevant laws, rules, or regulations applicable to GGL (India).
         </p>
@@ -109,12 +109,12 @@ const TermsOfUsePage: React.FC = () => {
           GGL (India) operates from India and manages the export of products and information. You agree to comply with all applicable restrictions and refrain from exporting or re-exporting any Content (including software or Services) to countries or individuals prohibited under India or other relevant export control laws or regulations. By accessing and downloading any Content (including software or Services) or Information, you confirm that you are not located in a country where such export is prohibited and are not a person or entity to whom such export is restricted. You are solely responsible for complying with the laws of your local jurisdiction and any other applicable laws concerning the import, export, or re-export of the Content (including software or Services).
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">Governing Law and Jurisdiction</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">Governing Law and Jurisdiction</h2>
         <p>
           To the maximum extent permitted by law, these Terms of Use are governed by the internal laws of India, and the courts in India shall have sole and exclusive jurisdiction over any disputes arising from or related to these Terms of Use.
         </p>
 
-        <h2 className="list-disc ml-6 mt-2">General</h2>
+        <h2 className="text-xl font-semibold mt-6 mb-2">General</h2>
         <p>
           You may not transfer these Terms of Use or any of your rights, interests, or obligations under these Terms of Use. If any provision of these Terms of Use is found invalid by a court of competent jurisdiction, the invalidity of that provision shall not affect the validity of the remaining provisions, which shall remain in full force and effect. No waiver of any of these Terms of Use shall be considered a continuing waiver of that term or condition or any other term or condition. You may retain a written copy of these Terms of Use by printing them for your records, waiving any other requirement for these Terms of Use to be evidenced by a written document.
         </p>
