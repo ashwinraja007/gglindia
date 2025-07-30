7c424df160b1bbf77afb941fb0fa953d845fd5d4
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-07-30 11:40:13 +0530

    Update QuickEnquiry.tsx

--- src/components/home/QuickEnquiry.tsx
+++ src/components/home/QuickEnquiry.tsx
@@ -60,7 +60,7 @@ export const QuickEnquiry = () => {
       formData.append("_captcha", "false");
       formData.append("_next", "https://www.gglindia.com/contact?success=true");
 
-      const response = await fetch("https://formsubmit.co/ajax/karthikjungleemara@gmail.com", {
+      const response = await fetch("https://formsubmit.co/ajax/sunder@ggl.sg", {
         method: "POST",
         body: formData,
       });
