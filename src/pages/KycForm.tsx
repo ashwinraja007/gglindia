355e300caae6111cefdd46f79c507f614cbfb769
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-11-24 15:55:00 +0530

    Create KycForm.tsx

---
+++ src/pages/KycForm.tsx
@@ -0,0 +1,13 @@
+export default function KycForm() {
+  return (
+    <iframe
+      src="https://www.tipscbse.com/kyc-details/kyc-details/kyc-details/"
+      style={{
+        width: "100%",
+        height: "1500px",
+        border: "none"
+      }}
+      title="KYC Form"
+    />
+  );
+}
