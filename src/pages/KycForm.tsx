c28556226852b5ed3ef5af3bdba31e07cac738e9
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-12-15 13:06:27 +0530

    Update KycForm.tsx

--- src/pages/KycForm.tsx
+++ src/pages/KycForm.tsx
@@ -1,7 +1,7 @@
 export default function KycForm() {
   return (
     <iframe
-      src="https://www.tipscbse.com/kyc-details/kyc-details/kyc-details/"
+      src="http://amassdubai.com/gglindia-kyc-details/"
       style={{
         width: "100%",
         height: "1500px",
