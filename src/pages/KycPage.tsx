95b5dc11ade9891a89b943ff6c6718d7f29a51cf
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-02-09 14:05:48 +0000

    commit

---
+++ src/pages/KycPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Header } from '@/components/layout/Header';
+import { Footer } from '@/components/layout/Footer';
+import SecureFrame from '@/components/SecureFrame';
+
+const KycPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8 pt-24">
+        <h1 className="text-2xl font-bold mb-6 text-gray-800">KYC Details</h1>
+        
+        <div className="h-[800px] w-full">
+          <SecureFrame 
+            targetUrl="http://www.tipscbse.com/kyc-details/kyc-details/" 
+            title="KYC Verification"
+          />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default KycPage;
