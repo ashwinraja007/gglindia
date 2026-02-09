d2b48a09c4a60625c5f613a65743e7223d68fb71
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-02-09 17:42:37 +0530

    Update KycForm.tsx

--- src/pages/KycForm.tsx
+++ src/pages/KycForm.tsx
@@ -1,14 +1,15 @@
-import { useEffect } from "react";
-
 const KycIframe = () => {
-  useEffect(() => {
-    window.open(
-      "http://www.amassdubai.com/gglindia-kyc-details/",
-      "_blank"
-    );
-  }, []);
-
-  return null;
+  return (
+    <iframe
+      src="https://www.tipscbse.com/kyc-details/kyc-details/"
+      title="KYC Details"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none"
+      }}
+    />
+  );
 };
 
 export default KycIframe;
